e1103014000d99c46d2cb0cbd96b98dbcbe5ad17
gjwoo960101 <example@com.net>
2025-02-26 19:05:20 +0900

    섹션3시작

---
+++ yalco/section3/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+import InfoCard from './pages/InfoCard'
+
+function App() {
+
+  return (
+    <>
+      <InfoCard 
+        title="Props in React"
+        content="Props pass data from one component to another."
+        author="Alice"
+      />
+      <InfoCard 
+        title="React Composition"
+        content="Composition makes your components more reusable"
+        author="Charlie"
+      />
+    </>
+  )
+}
+
+export default App
